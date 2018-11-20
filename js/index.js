1bdd51f3ee365e1ead37e41b8e6b7adf54039fb2
xueyue <994498178@qq.com>
2018-11-20 17:29:31 +0800

    hotmusic finish

--- js/index.js
+++ js/index.js
@@ -1,23 +1,25 @@
-$(function () {
+﻿$(function () {
 	setTimeout(function(){
 		$.get('./js/index.json').then(function(response){
 			let items = response
 			items.forEach((i)=>{
-				let $li = $(`
-				<li>
-					<a href="../song/song.html?id=${i.id}">
-						<h3>${i.name}</h3>
-						<p>${i.album}</p>
-						<span></span>
-					</a>
-				</li>			
-					`)
-				$('#lastestMusic').append($li)
+				if (i.id < 11) {
+					let $li = $(`
+					<li>
+						<a href="../song/song.html?id=${i.id}">
+							<h3>${i.name}</h3>
+							<p>${i.album}</p>
+							<span></span>
+						</a>
+					</li>			
+						`)
+					$('#lastestMusic').append($li)
+				}else{}
 			})
 			$('#lastestMusicloading').remove()
 		},function(){
 		})	
-	},1000)
+	},500)
 	$('.sitenav').on('click','ol.tabItems>li',function (e) {
 		let $li = $(e.currentTarget)
 		$li.children().addClass('active').parent().siblings().children().removeClass('active')
@@ -31,9 +33,46 @@ $(function () {
 			return
 		}
 		if (index === 1) {
-			$.get('./js/page2.json').then((response)=>{
-				$li.text(response.content)
+			$.get('./js/index.json').then(function(response){
 				$li.attr('data-downloaded','yes')
+				let myDate = new Date();
+				let month=myDate.getMonth()+1;
+				let date=myDate.getDate(); 		
+				$span = $('.tabContent .tab2 .hotTop .hotTime > span')
+				$span.text(month + '月' + date + '日')
+				$li.text(response.content)
+				let items = response
+				setTimeout(function () {
+					items.forEach((i)=>{
+						let id = i.id
+						if (id < 10) {
+							let $li = $(`
+							<li>
+								<a href="../song/song.html?id=${i.id}">
+									<h2>0${i.id}</h2>
+									<div><h3>${i.name}</h3>
+									<p>${i.album}</p>
+									<span></span></div>
+								</a>
+							</li>			
+								`)
+							$('#hotMusiclist').append($li)
+						}else{
+							let $li = $(`
+							<li>
+								<a href="../song/song.html?id=${i.id}">
+									<h2>${i.id}</h2>
+									<div><h3>${i.name}</h3>
+									<p>${i.album}</p>
+									<span></span></div>
+								</a>
+							</li>			
+								`)
+							$('#hotMusiclist').append($li)
+						}	
+					})
+					$('#hottMusicloading').remove()
+				},500)
 			})
 		}else if (index === 2) {
 			$.get('./js/page3.json').then((response)=>{
@@ -58,7 +97,7 @@ $(function () {
 				if (result.length !== 0) {
 					$('#output').text(result.map((r)=>r.name).join(','))
 				}else{
-					$('#output').text('没有结果')
+					$('#output').text('Ã»ÓÐ½á¹û')
 				}
 			})
 		},300)
@@ -67,10 +106,10 @@ $(function () {
 	function search(keyword) {
 		return new Promise((resolve,reject)=>{
 			var database = [
-				{"id":1, "name": "可乐"},
-				{"id":2, "name": "山丘 (Live)", },
-				{"id":3, "name": "光年之外", },
-				{"id":4, "name": "那些你很冒险的梦", }
+				{"id":1, "name": "¿ÉÀÖ"},
+				{"id":2, "name": "É½Çð (Live)", },
+				{"id":3, "name": "¹âÄêÖ®Íâ", },
+				{"id":4, "name": "ÄÇÐ©ÄãºÜÃ°ÏÕµÄÃÎ", }
 			]
 			let result = database.filter(function (item) {
 				return item.name.indexOf(keyword)>=0
